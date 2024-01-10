193f535b44e14cd0f6a1f54fc8d7b20e93d8b020
Alan Perez <79385309+matchagreen@users.noreply.github.com>
2024-01-09 22:29:50 -0500

    added some error_handling, and play command

--- src/bot.py
+++ src/bot.py
@@ -5,6 +5,12 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-@bot.command(name='ping', help='Responds with Hello')
-async def ping(ctx):
-    await ctx.send('pong')
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send('''`You forgot to provide a required argument. Send "/help {command}" for
+ details on how to run a command.
+`''')
+
+@bot.command(help='Plays a youtube link')
+async def play(ctx, url):
+    print('URL', url)
