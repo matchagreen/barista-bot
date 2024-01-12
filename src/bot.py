e1c2c4b3f9222035d42fc0d25d3c122a1b33e04b
Alan Perez <79385309+matchagreen@users.noreply.github.com>
2024-01-11 21:14:31 -0500

    this is very broken and very hard to read

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,4 @@
+from collections import deque
 import asyncio
 from pytube import YouTube
 import discord
@@ -6,6 +7,8 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
+
+playlist = deque()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -27,30 +30,69 @@ async def play(ctx, url):
         await ctx.send('`Please connect to the channel before requesting music to play. Bip bop`')
         return
 
-    if ctx.voice_client:
-        # Stop if already playing
-        vc = ctx.voice_client
+    # If already playing, replace
+    vc = ctx.voice_client
+    if vc:
         vc.stop()
-    else:
-        vc = await ctx.author.voice.channel.connect()
+        source = get_url_audio_source(url)
+        vc.play(source)
+        return
+
+    vc = await ctx.author.voice.channel.connect()
 
     try:
-        yt = YouTube(url)
-
-        audio_stream = yt.streams.filter(only_audio=True).first()
-        source = discord.FFmpegPCMAudio(audio_stream.url)
-        source.read()   # This prevents audio from speeding at the beginning
+        source = get_url_audio_source(url)
         vc.play(source, after=lambda error: print('Player error: ${e}') if error else None)
 
-        while vc.is_playing():
-            await asyncio.sleep(1)
+        # This will basically run forever as long as the bot keeps playing.
+        # One per instance of bot playing
+        while True:
+            if vc.is_paused() or vc.is_playing():
+                await asyncio.sleep(1)
+                continue
+
+            if playlist:
+                print(f'Playing next song: {url}')
+                source = get_url_audio_source(playlist.popleft())
+                print(f'Playlist: {[song for song in playlist]}')
+                vc.play(source)
+                continue
+
+            break
+
         await vc.disconnect()
 
     except Exception as e:
         await vc.disconnect()
         raise
 
+@bot.command(help='Adds to queue')
+async def play_last(ctx, url):
+    print(f'Adding {url} to playlist.')
+    playlist.append(url)
+    print(f'Playlist: {[song for song in playlist]}')
+
 @bot.command(help='Stops bot from playing audio')
 async def stop(ctx):
-    ctx.voice_client.stop()
-    await ctx.send('`Stopping audio. Bip bop`')
+    vc = ctx.voice_client
+    if not vc:
+        return
+
+    vc.stop()
+
+@bot.command(help='Bot pauses playing audio')
+async def pause(ctx):
+    vc = ctx.voice_client
+    if not vc:
+        return
+
+    vc.pause()
+
+def get_url_audio_source(url: str):
+    yt = YouTube(url)
+
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    source = discord.FFmpegPCMAudio(audio_stream.url)
+    source.read()   # This prevents audio from speeding at the beginning
+
+    return source
